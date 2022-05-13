b0c43b5bc469289731effa8bd9aff89d50398bbe
Steven Le <stevenle08@gmail.com>
2022-05-13 05:25:05 -0700

    Add Select example

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,6 +3,7 @@ import { useState } from "preact/hooks";
 import { useListState, randomId } from "@mantine/hooks";
 import { Cross1Icon, PlusIcon } from "@modulz/radix-icons";
 import {
+  Select,
   Title,
   Text,
   Checkbox,
@@ -138,6 +139,17 @@ export function TodoList() {
               setNewItem("");
             }
           }}
+        />
+
+        <Select
+          label="Your favorite framework/library"
+          placeholder="Pick one"
+          data={[
+            { value: 'react', label: 'React' },
+            { value: 'ng', label: 'Angular' },
+            { value: 'svelte', label: 'Svelte' },
+            { value: 'vue', label: 'Vue' },
+          ]}
         />
       </Container>
 
